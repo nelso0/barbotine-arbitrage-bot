2bb9690b57bf887dc81bac6d64b1fff194a6a172
Nelso <87613463+nelso0@users.noreply.github.com>
2023-03-28 22:57:44 +0200

    jk

--- exchange_config.py
+++ exchange_config.py
@@ -3,9 +3,20 @@ import requests
 import time
 
 ex = {
-    'kucoin':ccxt.kucoin(),
-    'binance':ccxt.binance(),
-    'okx':ccxt.okx(),
+    'kucoin':ccxt.kucoin({
+        'apiKey':'here',
+        'secret':'here',
+        'password':'here'
+    }),
+    'binance':ccxt.binance({
+        'apiKey':'here',
+        'secret':'here',
+    }),
+    'okx':ccxt.okx({
+        'apiKey':'here',
+        'secret':'here',
+        'password':'here'
+    }),
     
     # 'another_exchange_here':ccxt.other_exchange({
     #     'apiKey':'here',
@@ -13,27 +24,22 @@ ex = {
     # }),
 }
 
-# put maker fees of your exchanges in "receive" (only if it's a quote and not base fee)
 
-fees = {
-    'binance' : {'give':0,"receive":0.001},
-    'kucoin' : {'give':0,"receive":0.001},
-    'okx' : {'give':0,"receive":0.0008},
-    'another_exchange_here' : {'give':0,"receive":0},
+fees = { # put maker fees of your exchanges in 'receive' (only if it's a quote and not base fee)
+    'binance' : {'receive':0.001},
+    'kucoin' : {'receive':0.001},
+    'okx' : {'receive':0.0008},
+    'another_exchange_here' : {'receive':0},
 }
 
-# telegram API to send everything to you
-
-apiToken = 'here'
+telegram_sending = False
+apiToken = 'here' # telegram API to send everything to you, don't fill if you don't want telegram (False = not activated by default)
 chatID = 'here'
 
-# minimum of profits to take the opportunity
-# default to 0 so it takes the opportunity even if this is a 0.001 USD profit. (all fees are already included in the calculation, so it's actual profit)
+first_orders_fill_timeout = 0 # ONLY FOR FULL VERSION       # if equals to 0 = no timeout, check indefinitely to fill first orders. Put a value for the timeout in seconds if you want to have a timeout.
 
-criteria_pct = 0
+criteria_pct = 0 # minimum of profits to take the opportunity, default to 0 so it takes the opportunity even if this is a 0.001 USD profit. (all fees are already included in the calculation, so it's actual profit)
 criteria_usd = 0
-
-# some useful functions here
 
 def moy(list):
     moy=0
@@ -43,16 +49,18 @@ def moy(list):
 def send_to_telegram(message):
     apiURL = f'https://api.telegram.org/bot{apiToken}/sendMessage'
     try:
-        pass
-        #response = requests.post(apiURL, json={'chat_id': chatID, 'text': message}) # put this if you want to send telegram messages.
+        if telegram_sending:
+            response = requests.post(apiURL, json={'chat_id': chatID, 'text': message}) # put this if you want to send telegram messages.
+        else:
+            pass
     except Exception as e:
         print(e)
 def append_new_line(file_name, text_to_append):
-    with open(file_name, "a+") as file_object:
+    with open(file_name, 'a+') as file_object:
         file_object.seek(0)
         data = file_object.read(100)
         if len(data) > 0:
-            file_object.turtle.write("\n")
+            file_object.turtle.write('\n')
         file_object.turtle.write(text_to_append)
 def printandtelegram(message):
     print(message)
@@ -61,6 +69,6 @@ def get_balance(exchange,symbol):
     if symbol[-5:] == '/USDT':
         symbol = symbol[:-5]
     balance=ex[exchange].fetch_balance()
-    if symbol in balance["free"] != 0:
+    if symbol in balance['free'] != 0:
         return balance['free'][symbol]
     else: return 0
