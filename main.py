f0deeaaeaf38944e7038ae7621f27439791b71ce
nelso <87613463+nelso0@users.noreply.github.com>
2023-04-05 17:10:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ import subprocess
 import time
 import sys
 import os
-
+from colorama import Style, init
+init()
+from exchange_config import *
+sys.stdin.reconfigure(encoding="utf-8")
+sys.stdout.reconfigure(encoding="utf-8")
 print("""
 █▄▄ ▄▀█ █▀█ █▄▄ █▀█ ▀█▀ █ █▄░█ █▀▀   ▄▀█ █▀█ █▄▄ █ ▀█▀ █▀█ ▄▀█ █▀▀ █▀▀   █▀ █▄█ █▀ ▀█▀ █▀▀ █▀▄▀█
 █▄█ █▀█ █▀▄ █▄█ █▄█ ░█░ █ █░▀█ ██▄   █▀█ █▀▄ █▄█ █ ░█░ █▀▄ █▀█ █▄█ ██▄   ▄█ ░█░ ▄█ ░█░ ██▄ █░▀░█""")
@@ -19,9 +23,9 @@ ex1=args[4]
 ex2=args[5]
 ex3=args[6]
 i=0
-with open(f"start_balance-{balance}.txt","w") as f:
+with open(f"start_balance.txt","w") as f:
     f.write(balance)
-with open(f"balance-{balance}.txt","w") as f:
+with open(f"balance.txt","w") as f:
     f.write(balance)
 
 while True:
@@ -30,50 +34,50 @@ while True:
     if mode == "fake-money":
         if len(args)<8:
 
-            print(f"[{time.strftime('%H:%M:%S', time.gmtime(time.time()))}] Searching symbol... (can take some minutes)")
-            p=subprocess.run(['python', "best-symbol.py"])
+            print(f"{Style.DIM}[{time.strftime('%H:%M:%S', time.gmtime(time.time()))}]{Style.RESET_ALL} Searching symbol... (can take some minutes)")
+            p=subprocess.run([how_do_you_usually_launch_python, "best-symbol.py"])
             with open('symbol.txt') as f:
                 symbol=f.read()
-            print(f"[{time.strftime('%H:%M:%S', time.gmtime(time.time()))}] Crypto pair is: {symbol}")
-            p=subprocess.run(['python', "bot-fake-money.py",symbol,balance,renew,symbol,ex1,ex2,ex3])
-            with open(f"balance-{balance}.txt") as f:
+            print(f"{Style.DIM}[{time.strftime('%H:%M:%S', time.gmtime(time.time()))}]{Style.RESET_ALL} Crypto pair is: {symbol}")
+            p=subprocess.run([how_do_you_usually_launch_python, "bot-fake-money.py",symbol,balance,renew,symbol,ex1,ex2,ex3])
+            with open(f"balance.txt") as f:
                 balance=f.read()
 
         if len(args)>=8:
-            p=subprocess.run(['python',"bot-fake-money.py",symbol,balance,renew,symbol,ex1,ex2,ex3])
-            with open(f"balance-{balance}.txt") as f:
+            p=subprocess.run([how_do_you_usually_launch_python,"bot-fake-money.py",symbol,balance,renew,symbol,ex1,ex2,ex3])
+            with open(f"balance.txt") as f:
                 balance=f.read()
     elif mode == "classic":
         if len(args)<8:
 
-            print(f"[{time.strftime('%H:%M:%S', time.gmtime(time.time()))}] Searching symbol... (can take some minutes)")
-            p=subprocess.run(['python', "best-symbol.py"])
+            print(f"{Style.DIM}[{time.strftime('%H:%M:%S', time.gmtime(time.time()))}]{Style.RESET_ALL} Searching symbol... (can take some minutes)")
+            p=subprocess.run([how_do_you_usually_launch_python, "best-symbol.py"])
             with open('symbol.txt') as f:
                 symbol=f.read()
-            print(f"[{time.strftime('%H:%M:%S', time.gmtime(time.time()))}] Crypto pair is: {symbol}")
-            p=subprocess.run(['python', "bot-classic.py",symbol,balance,renew,symbol,ex1,ex2,ex3])
-            with open(f"balance-{balance}.txt") as f:
+            print(f"{Style.DIM}[{time.strftime('%H:%M:%S', time.gmtime(time.time()))}]{Style.RESET_ALL} Crypto pair is: {symbol}")
+            p=subprocess.run([how_do_you_usually_launch_python, "bot-classic.py",symbol,balance,renew,symbol,ex1,ex2,ex3])
+            with open(f"balance.txt") as f:
                 balance=f.read()
 
         if len(args)>=8:
-            p=subprocess.run(['python',"bot-classic.py",symbol,balance,renew,symbol,ex1,ex2,ex3])
-            with open(f"balance-{balance}.txt") as f:
+            p=subprocess.run([how_do_you_usually_launch_python,"bot-classic.py",symbol,balance,renew,symbol,ex1,ex2,ex3])
+            with open(f"balance.txt") as f:
                 balance=f.read()
     elif mode == "delta-neutral":
         if len(args)<8:
 
-            print(f"[{time.strftime('%H:%M:%S', time.gmtime(time.time()))}] Searching symbol... (can take some minutes)")
-            p=subprocess.run(['python', "best-symbol.py"])
+            print(f"{Style.DIM}[{time.strftime('%H:%M:%S', time.gmtime(time.time()))}]{Style.RESET_ALL} Searching symbol... (can take some minutes)")
+            p=subprocess.run([how_do_you_usually_launch_python, "best-symbol.py"])
             with open('symbol.txt') as f:
                 symbol=f.read()
-            print(f"[{time.strftime('%H:%M:%S', time.gmtime(time.time()))}] Crypto pair is: {symbol}")
-            p=subprocess.run(['python', "bot-delta-neutral.py",symbol,balance,renew,symbol,ex1,ex2,ex3])
-            with open(f"balance-{balance}.txt") as f:
+            print(f"{Style.DIM}[{time.strftime('%H:%M:%S', time.gmtime(time.time()))}]{Style.RESET_ALL} Crypto pair is: {symbol}")
+            p=subprocess.run([how_do_you_usually_launch_python, "bot-delta-neutral.py",symbol,balance,renew,symbol,ex1,ex2,ex3])
+            with open(f"balance.txt") as f:
                 balance=f.read()
 
         if len(args)>=8:
-            p=subprocess.run(['python',"bot-delta-neutral.py",symbol,balance,renew,symbol,ex1,ex2,ex3])
-            with open(f"balance-{balance}.txt") as f:
+            p=subprocess.run([how_do_you_usually_launch_python,"bot-delta-neutral.py",symbol,balance,renew,symbol,ex1,ex2,ex3])
+            with open(f"balance.txt") as f:
                 balance=f.read()
     else:
         print(f"Mode input is incorrect.")
