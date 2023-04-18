aeecbac8703c9ac63c500afee6eaede0747ce363
nelso <87613463+nelso0@users.noreply.github.com>
2023-04-18 17:33:58 +0200

    Update best-symbol.py

--- best-symbol.py
+++ best-symbol.py
@@ -1,17 +1,18 @@
 # it's sloppy; needs serious improvement.
 import time
+import sys
 from exchange_config import *
 st = time.time()
 binancePairs = []
-marketsBinance = ex['binance'].load_markets()
+marketsBinance = ex[sys.argv[1]].load_markets()
 for m in marketsBinance:
     binancePairs.append(m)
 kucoinPairs = []
-marketsKucoin = ex['kucoin'].load_markets()
+marketsKucoin = ex[sys.argv[2]].load_markets()
 for m in marketsKucoin:
     kucoinPairs.append(m)
 OkxPairs = []
-marketsOKX = ex['okx'].load_markets()
+marketsOKX = ex[sys.argv[3]].load_markets()
 for m in marketsOKX:
     OkxPairs.append(m)
 OKXset = set(OkxPairs)
@@ -21,13 +22,13 @@ if (OKXset & Binanceset & Kucoinset):
     list1 = list(OKXset & Binanceset & Kucoinset)
 list2 = []
 for n in list1:
-    ticker = ex['binance'].fetch_ticker(n)
+    ticker = ex[sys.argv[1]].fetch_ticker(n)
     if n[-4:] == 'USDT' and float(ticker["info"]["volume"])*float(ticker["last"])>1200000 and n!='MIR/USDT':
         list2.append(n)
         time.sleep(1)
 finalDic={}
 for x in list2:
-    pct = round((ex['binance'].fetch_ticker(x)['percentage']+ex['kucoin'].fetch_ticker(x)['percentage']+ex['okx'].fetch_ticker(x)['percentage'])/3,3)
+    pct = round((ex[sys.argv[1]].fetch_ticker(x)['percentage']+ex[sys.argv[2]].fetch_ticker(x)['percentage']+ex[sys.argv[3]].fetch_ticker(x)['percentage'])/3,3)
     finalDic[x] = pct
 max_pct = max([finalDic[n] for n in finalDic])
 max_symbol = list(finalDic.keys())[list(finalDic.values()).index(max_pct)]
