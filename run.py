26e0a286fc9e89266ad431400e423c67083dd566
nelso <87613463+nelso0@users.noreply.github.com>
2023-07-08 15:41:50 +0200

    Update run.py

--- run.py
+++ run.py
@@ -13,13 +13,13 @@ def emergency_convert_list(pair_to_sell,exlist):
         try:
             if ex[echange].has['cancelAllOrders'] and ex[echange].fetchOpenOrders(pair_to_sell) != []:
                 ex[echange].cancelAllOrders(pair_to_sell)
-                print(f"{Style.DIM}[{time.strftime('%H:%M:%S', time.gmtime(time.time()))}]{Style.RESET_ALL} Successfully canceled all orders on {echange}.")
+                print(f"{Style.DIM}{get_time()}{Style.RESET_ALL} Successfully canceled all orders on {echange}.")
             bal = get_balance(echange,pair_to_sell)
             bal-=bal*0.01
             if bal>(float(10)/float(ex[echange].fetch_ticker(pair_to_sell)['last'])):
                 ex[echange].createMarketSellOrder(symbol=pair_to_sell,amount=round(bal,3))
-                print(f"{Style.DIM}[{time.strftime('%H:%M:%S', time.gmtime(time.time()))}]{Style.RESET_ALL} Successfully sold {bal} {pair_to_sell[:len(pair_to_sell)-5]} on {echange}.")
-            else: print(f"{Style.DIM}[{time.strftime('%H:%M:%S', time.gmtime(time.time()))}]{Style.RESET_ALL} Not enough {pair_to_sell[:len(pair_to_sell)-5]} on {echange}.")
+                print(f"{Style.DIM}{get_time()}{Style.RESET_ALL} Successfully sold {bal} {pair_to_sell[:len(pair_to_sell)-5]} on {echange}.")
+            else: print(f"{Style.DIM}{get_time()}{Style.RESET_ALL} Not enough {pair_to_sell[:len(pair_to_sell)-5]} on {echange}.")
             i+=1
         except Exception as e:
             print(f'{Style.DIM}[{time.strftime("%H:%M:%S", time.gmtime(time.time()))}]{Style.RESET_ALL} Problem on {echange}. Error:    {e}')
@@ -45,10 +45,7 @@ try:
             subprocess.run([how_do_you_usually_launch_python,f"main.py",output[0],output[1],output[2],output[3],output[4],output[5],output[6]])
 
     else:
-        print("""
-    █▄▄ ▄▀█ █▀█ █▄▄ █▀█ ▀█▀ █ █▄░█ █▀▀   ▄▀█ █▀█ █▄▄ █ ▀█▀ █▀█ ▄▀█ █▀▀ █▀▀   █▀ █▄█ █▀ ▀█▀ █▀▀ █▀▄▀█
-    █▄█ █▀█ █▀▄ █▄█ █▄█ ░█░ █ █░▀█ ██▄   █▀█ █▀▄ █▄█ █ ░█░ █▀▄ █▀█ █▄█ ██▄   ▄█ ░█░ ▄█ ░█░ ██▄ █░▀░█""")
-        print(" \nUnder Attribution-NonCommercial 4.0 International (CC BY-NC 4.0) license, for personal-use only.\n \nLink for full version: https://get.barbotine.capital/product/full-package-of-barbotine\n \nGithub: nelso0\nTwitter: @nelsodot\n")
+        
         if len(sys.argv) < 7 or len(sys.argv) > 8:
             print("Not correctly configured. Usage:\n \n{how_do_you_usually_launch_python} run.py <mode> <symbol-renew-time> <usdt-to-use> <exchange1> <exchange2> <exchange3> [crypto-pair]\n")
             sys.exit(1)
@@ -61,6 +58,10 @@ try:
         ex1=args[4]
         ex2=args[5]
         ex3=args[6]
+        print("""
+    █▄▄ ▄▀█ █▀█ █▄▄ █▀█ ▀█▀ █ █▄░█ █▀▀   ▄▀█ █▀█ █▄▄ █ ▀█▀ █▀█ ▄▀█ █▀▀ █▀▀   █▀ █▄█ █▀ ▀█▀ █▀▀ █▀▄▀█
+    █▄█ █▀█ █▀▄ █▄█ █▄█ ░█░ █ █░▀█ ██▄   █▀█ █▀▄ █▄█ █ ░█░ █▀▄ █▀█ █▄█ ██▄   ▄█ ░█░ ▄█ ░█░ ██▄ █░▀░█""")
+        print(f" \n{Fore.CYAN}FAKE-MONEY VERSION{Style.RESET_ALL}\n \nLink for real money version: https://barbotine.capital/purchase-arbitrage\n \nGithub: nelso0\nTwitter: @nelsorex\nDiscord: nelsorex\n")
         i=0
         with open(f"start_balance.txt","w") as f:
             f.write(balance)
@@ -75,11 +76,11 @@ try:
             if mode == "fake-money":
                 if len(args)<8:
 
-                    print(f"{Style.DIM}[{time.strftime('%H:%M:%S', time.gmtime(time.time()))}]{Style.RESET_ALL} Searching symbol... (can take some minutes)")
+                    print(f"{Style.DIM}{get_time()}{Style.RESET_ALL} Searching symbol... (can take some minutes)")
                     p=subprocess.run([how_do_you_usually_launch_python, "best-symbol.py",ex1,ex2,ex3])
                     with open('symbol.txt') as f:
                         symbol=f.read()
-                    print(f"{Style.DIM}[{time.strftime('%H:%M:%S', time.gmtime(time.time()))}]{Style.RESET_ALL} Crypto pair is: {symbol}")
+                    print(f"{Style.DIM}{get_time()}{Style.RESET_ALL} Crypto pair is: {symbol}")
                     p=subprocess.run([how_do_you_usually_launch_python, "bot-fake-money.py",symbol,balance,renew,symbol,ex1,ex2,ex3])
                     with open(f"balance.txt") as f:
                         balance=f.read()
@@ -91,11 +92,11 @@ try:
             elif mode == "classic":
                 if len(args)<8:
 
-                    print(f"{Style.DIM}[{time.strftime('%H:%M:%S', time.gmtime(time.time()))}]{Style.RESET_ALL} Searching symbol... (can take some minutes)")
+                    print(f"{Style.DIM}{get_time()}{Style.RESET_ALL} Searching symbol... (can take some minutes)")
                     p=subprocess.run([how_do_you_usually_launch_python, "best-symbol.py",ex1,ex2,ex3])
                     with open('symbol.txt') as f:
                         symbol=f.read()
-                    print(f"{Style.DIM}[{time.strftime('%H:%M:%S', time.gmtime(time.time()))}]{Style.RESET_ALL} Crypto pair is: {symbol}")
+                    print(f"{Style.DIM}{get_time()}{Style.RESET_ALL} Crypto pair is: {symbol}")
                     p=subprocess.run([how_do_you_usually_launch_python, "bot-classic.py",symbol,balance,renew,symbol,ex1,ex2,ex3])
                     with open(f"balance.txt") as f:
                         balance=f.read()
@@ -107,11 +108,11 @@ try:
             elif mode == "delta-neutral":
                 if len(args)<8:
 
-                    print(f"{Style.DIM}[{time.strftime('%H:%M:%S', time.gmtime(time.time()))}]{Style.RESET_ALL} Searching symbol... (can take some minutes)")
+                    print(f"{Style.DIM}{get_time()}{Style.RESET_ALL} Searching symbol... (can take some minutes)")
                     p=subprocess.run([how_do_you_usually_launch_python, "best-symbol.py",ex1,ex2,ex3])
                     with open('symbol.txt') as f:
                         symbol=f.read()
-                    print(f"{Style.DIM}[{time.strftime('%H:%M:%S', time.gmtime(time.time()))}]{Style.RESET_ALL} Crypto pair is: {symbol}")
+                    print(f"{Style.DIM}{get_time()}{Style.RESET_ALL} Crypto pair is: {symbol}")
                     p=subprocess.run([how_do_you_usually_launch_python, "bot-delta-neutral.py",symbol,balance,renew,symbol,ex1,ex2,ex3])
                     with open(f"balance.txt") as f:
                         balance=f.read()
@@ -129,7 +130,7 @@ except KeyboardInterrupt:
         print(" \n \n \n")
         answered = False
         while answered == False:
-            inp = input(f"{Style.DIM}[{time.strftime('%H:%M:%S', time.gmtime(time.time()))}]{Style.RESET_ALL} CTRL+C was pressed. Do you want to sell all crypto back to USDT? (y)es / (n)o\n \ninput: ")
+            inp = input(f"{Style.DIM}{get_time()}{Style.RESET_ALL} CTRL+C was pressed. Do you want to sell all crypto back to USDT? (y)es / (n)o\n \ninput: ")
             if inp == "y" or inp == "Y" or inp == "yes" or inp == "Yes":
                 answered = True
                 emergency_convert_list(symbol,[ex1,ex2,ex3])
