e6b1796a79b69d71a9de648263618d995d7ce007
nelso <87613463+nelso0@users.noreply.github.com>
2023-09-04 12:58:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,16 +10,13 @@ sys.stdout.reconfigure(encoding="utf-8")
 print("""
     █▄▄ ▄▀█ █▀█ █▄▄ █▀█ ▀█▀ █ █▄░█ █▀▀   ▄▀█ █▀█ █▄▄ █ ▀█▀ █▀█ ▄▀█ █▀▀ █▀▀   █▀ █▄█ █▀ ▀█▀ █▀▀ █▀▄▀█
     █▄█ █▀█ █▀▄ █▄█ █▄█ ░█░ █ █░▀█ ██▄   █▀█ █▀▄ █▄█ █ ░█░ █▀▄ █▀█ █▄█ ██▄   ▄█ ░█░ ▄█ ░█░ ██▄ █░▀░█""")
-print(f" \n{Fore.BLUE}{Style.BRIGHT}FAKE-MONEY VERSION{Style.RESET_ALL}\n \nGithub: nelso0\nTwitter: @nelsorex\nDiscord: nelsorex\n")
+print(f" \n{Fore.YELLOW}{Style.BRIGHT}FULL VERSION{Style.RESET_ALL}\n \nGithub: nelso0\nTwitter: @nelsorex\nDiscord: nelsorex\n")
 args = sys.argv
 mode = args[1]
 balance = args[3]
-if len(args)>=8:
-    symbol=args[7]
+symbol=args[4]
 renew=args[2]
-ex1=args[4]
-ex2=args[5]
-ex3=args[6]
+ex_list=args[5]
 i=0
 with open(f"start_balance.txt","w") as f:
     f.write(balance)
@@ -30,53 +27,17 @@ while True:
     if i>=1 and p.returncode==1:
         sys.exit(1)
     if mode == "fake-money":
-        if len(args)<8:
-
-            print(f"{Style.DIM}{get_time()}{Style.RESET_ALL} Searching symbol... (can take some minutes)")
-            p=subprocess.run([how_do_you_usually_launch_python, "best-symbol.py",ex1,ex2,ex3])
-            with open('symbol.txt') as f:
-                symbol=f.read()
-            print(f"{Style.DIM}{get_time()}{Style.RESET_ALL} Crypto pair is: {symbol}")
-            p=subprocess.run([how_do_you_usually_launch_python, "bot-fake-money.py",symbol,balance,renew,symbol,ex1,ex2,ex3])
-            with open(f"balance.txt") as f:
-                balance=f.read()
-
-        if len(args)>=8:
-            p=subprocess.run([how_do_you_usually_launch_python,"bot-fake-money.py",symbol,balance,renew,symbol,ex1,ex2,ex3])
-            with open(f"balance.txt") as f:
-                balance=f.read()
+        p=subprocess.run([how_do_you_usually_launch_python,"bot-fake-money.py",symbol,balance,renew,symbol,ex_list])
+        with open(f"balance.txt") as f:
+            balance=f.read()
     elif mode == "classic":
-        if len(args)<8:
-
-            print(f"{Style.DIM}{get_time()}{Style.RESET_ALL} Searching symbol... (can take some minutes)")
-            p=subprocess.run([how_do_you_usually_launch_python, "best-symbol.py",ex1,ex2,ex3])
-            with open('symbol.txt') as f:
-                symbol=f.read()
-            print(f"{Style.DIM}{get_time()}{Style.RESET_ALL} Crypto pair is: {symbol}")
-            p=subprocess.run([how_do_you_usually_launch_python, "bot-classic.py",symbol,balance,renew,symbol,ex1,ex2,ex3])
-            with open(f"balance.txt") as f:
-                balance=f.read()
-
-        if len(args)>=8:
-            p=subprocess.run([how_do_you_usually_launch_python,"bot-classic.py",symbol,balance,renew,symbol,ex1,ex2,ex3])
-            with open(f"balance.txt") as f:
-                balance=f.read()
+        p=subprocess.run([how_do_you_usually_launch_python,"bot-classic.py",symbol,balance,renew,symbol,ex_list])
+        with open(f"balance.txt") as f:
+            balance=f.read()
     elif mode == "delta-neutral":
-        if len(args)<8:
-
-            print(f"{Style.DIM}{get_time()}{Style.RESET_ALL} Searching symbol... (can take some minutes)")
-            p=subprocess.run([how_do_you_usually_launch_python, "best-symbol.py",ex1,ex2,ex3])
-            with open('symbol.txt') as f:
-                symbol=f.read()
-            print(f"{Style.DIM}{get_time()}{Style.RESET_ALL} Crypto pair is: {symbol}")
-            p=subprocess.run([how_do_you_usually_launch_python, "bot-delta-neutral.py",symbol,balance,renew,symbol,ex1,ex2,ex3])
-            with open(f"balance.txt") as f:
-                balance=f.read()
-
-        if len(args)>=8:
-            p=subprocess.run([how_do_you_usually_launch_python,"bot-delta-neutral.py",symbol,balance,renew,symbol,ex1,ex2,ex3])
-            with open(f"balance.txt") as f:
-                balance=f.read()
+        p=subprocess.run([how_do_you_usually_launch_python,"bot-delta-neutral.py",symbol,balance,renew,symbol,ex_list])
+        with open(f"balance.txt") as f:
+            balance=f.read()
     else:
         print(f"Mode input is incorrect.")
         sys.exit(1)
