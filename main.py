3fe8ed7b6def49b01e79d35dec3fa063374bffc0
nelso <87613463+nelso0@users.noreply.github.com>
2023-09-11 18:07:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,10 +13,16 @@ print("""
 print(f" \n{Fore.BLUE}{Style.BRIGHT}DEMO VERSION{Style.RESET_ALL}\n \nGithub: nelso0\nTwitter: @nelsorex\nDiscord: nelsorex\n")
 args = sys.argv
 mode = args[1]
-balance = args[3]
-symbol=args[4]
-renew=args[2]
-ex_list=args[5]
+if renewal:
+    balance = args[3]
+    symbol=args[4]
+    renew=args[2]
+    ex_list=args[5]
+else:
+    balance = args[2]
+    symbol=args[3]
+    renew="525600"
+    ex_list=args[4]
 i=0
 with open(f"start_balance.txt","w") as f:
     f.write(balance)
