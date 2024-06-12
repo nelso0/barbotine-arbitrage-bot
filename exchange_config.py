a57eb008e3d2a9ee272f39bdcd4deb9b4b44c461
nelso <87613463+nelso0@users.noreply.github.com>
2024-06-13 01:24:17 +0200

    Update exchange_config.py

--- exchange_config.py
+++ exchange_config.py
@@ -1,37 +1,40 @@
 import ccxt as ccxt
 import requests
-import time
 import pytz
+from colorama import Style,Fore
 import datetime
 
-# The bot seems complicated? I swear it's not, just try! (and contact me if you have an error, it's probably a silly one :)
+# The bot seems complicated? It's not, just try! (and contact me if you have an error, it's probably a silly one :)
 
-telegram_sending = False
-ctrl_c_handling = True
-renewal = False # you can enable session renewal here, default: False
+# Link for all parameters explanation: https://docs.google.com/document/d/1qSXVCaoXEvL_5YwHjUKDa-ml3A8d8h-fnU0VhA9xLrY/edit?usp=sharing 
 
-how_do_you_usually_launch_python = 'python3' # the command you put in the terminal/cmd to launch python. Usually: python, python3, py...
+renewal = False # (1)
+delta_neutral = False # (2)
+timezone = 'Europe/Paris'
+how_do_you_usually_launch_python = 'python3' # (3)
 
-# you have to put your api keys if you want to use real money modes. To do that, add "{'apiKey':'here','secret':'here'}" between the parenthesis () by replacing 'here' with the api key and the secret key.
-# read the instructions on the google drive link you received for more details and screenshots.
-ex = {
-    'kucoin':ccxt.kucoin(),
-    'binance':ccxt.binance(),
-    'okx':ccxt.okx(),
-    'poloniex':ccxt.poloniex(),
-    # 'another_exchange_here':ccxt.other_exchange({
+# (4)
+exchanges = {
+    'kucoin':{},
+    'binance':{},
+    'okx':{},
+    'poloniex':{},
+    # 'another_exchange_here':{
     #     'apiKey':'here',
     #     'secret':'here',
-    # }),
+    # },
 }
 
-apiToken = 'here' # telegram API to send everything to you, don't fill if you don't want telegram
+telegram_sending = False
+apiToken = 'here' # (5)
 chatID = 'here'
 
-first_orders_fill_timeout = 0 # put a value for the timeout in minutes. 0 means desactivated (default)
+criteria_pct = 0 # (6)
+criteria_usd = 0 # (7)
 
-criteria_pct = 0 # minimum of price difference in % to take the opportunity
-criteria_usd = 0
+first_orders_fill_timeout = 0 # (8)
+
+# ------------------------------------ FUNCTIONS (you can ignore) ------------------------------------
 
 def moy(list1):
     moy=0
@@ -42,6 +45,8 @@ def send_to_telegram(message):
     message = message.replace("[2m","")
     message = message.replace("[0m","")
     message = message.replace("[2m","")
+    message = message.replace("[32m","")
+    message = message.replace("[31m","")
     apiURL = f'https://api.telegram.org/bot{apiToken}/sendMessage'
     try:
         if telegram_sending:
@@ -63,12 +68,61 @@ def append_list_file(fichier, nouvel_element):
     with open(fichier, 'w') as file:
         file.write(str(liste))
 def append_new_line(file_name, text_to_append):
+    import os
+    """Appends a new line to a text file, creating directories if necessary.
+
+    Args:
+        file_name: The path to the file, including any directories.
+        text_to_append: The text to append to the file.
+    """
+    
+    dir_name = os.path.dirname(file_name)
+    if dir_name:
+        os.makedirs(dir_name, exist_ok=True)
+
     with open(file_name, 'a+') as file_object:
         file_object.seek(0)
         data = file_object.read(100)
         if len(data) > 0:
-            file_object.turtle.write('\n')
-        file_object.turtle.write(text_to_append)
+            file_object.write('\n')
+        file_object.write(text_to_append)
+def printerror(**args):
+    if 'm' in list(args.keys()):
+        print(f"{get_time()}{Fore.RED}{Style.BRIGHT}Error: {args['m']}{Style.RESET_ALL}")
+        append_new_line("logs/logs.txt",f"{get_time_blank()} ERROR: {args['m']}")
+    else:
+        print(f"{get_time()}{Fore.RED}{Style.BRIGHT}Error.{Style.RESET_ALL}")
+
+    if 'name_of_data' in list(args.keys()) and 'data' in list(args.keys()) and 'm' in list(args.keys()):
+        append_new_line("logs/logs.txt",f"{get_time_blank()} ERROR: {args['m']} | {args['name_of_data']}: {args['data']}")
+
+    elif 'name_of_data' in list(args.keys()) and 'data' in list(args.keys()):
+        append_new_line("logs/logs.txt",f"{get_time_blank()} ERROR | {args['name_of_data']}: {args['data']}")
+def emergency_convert_list(pair_to_sell,exlist):
+    i=0
+    for echange in exlist:
+        try:
+            if ex[echange].has['cancelAllOrders'] and ex[echange].fetchOpenOrders(pair_to_sell) != []:
+                ex[echange].cancelAllOrders(pair_to_sell)
+                print(f"{get_time()} Successfully canceled all orders on {echange}.")
+                append_new_line('logs/logs.txt',f"{get_time_blank()} INFO: successfully canceled all orders on {echange}.")
+            bal = get_balance(echange,pair_to_sell)
+            m = ex[echange].load_markets()
+            t=ex[echange].fetch_ticker(pair_to_sell)
+            m[pair_to_sell]['limits']['cost']['min'] = 0 if type(m[pair_to_sell]['limits']['cost']['min'])!=float and type(m[pair_to_sell]['limits']['cost']['min'])!=int else m[pair_to_sell]['limits']['cost']['min']
+            m[pair_to_sell]['limits']['amount']['min'] = 0 if type(m[pair_to_sell]['limits']['amount']['min'])!=float and type(m[pair_to_sell]['limits']['amount']['min'])!=int else m[pair_to_sell]['limits']['amount']['min']
+            m[pair_to_sell]['limits']['cost']['max'] = 10e13 if type(m[pair_to_sell]['limits']['cost']['max'])!=float and type(m[pair_to_sell]['limits']['cost']['max'])!=int else m[pair_to_sell]['limits']['cost']['max']
+            m[pair_to_sell]['limits']['amount']['max'] = 10e13 if type(m[pair_to_sell]['limits']['amount']['max'])!=float and type(m[pair_to_sell]['limits']['amount']['max'])!=int else m[pair_to_sell]['limits']['amount']['max']
+            if (bal>(m[pair_to_sell]['limits']['cost']['min']/float(t['last'])) and bal>m[pair_to_sell]['limits']['amount']['min']) and (bal<(m[pair_to_sell]['limits']['cost']['max']/float(t['last'])) and bal<m[pair_to_sell]['limits']['amount']['max']):
+                ex[echange].createMarketSellOrder(symbol=pair_to_sell,amount=bal)
+                print(f"{get_time()} Successfully sold {bal} {pair_to_sell[:len(pair_to_sell)-5]} on {echange}.")
+                append_new_line('logs/logs.txt',f"{get_time_blank()} INFO: Successfully sold {bal} {pair_to_sell[:len(pair_to_sell)-5]} on {echange}.")
+            else: 
+                print(f"{get_time()} Not enough {pair_to_sell[:len(pair_to_sell)-5]} on {echange}.")
+                append_new_line('logs/logs.txt',f"{get_time_blank()} INFO: Successfully sold {bal} {pair_to_sell[:len(pair_to_sell)-5]} on {echange}.")
+            i+=1
+        except Exception as e:
+            printerror(m=f'{get_time()} Error while selling {pair_to_sell} on {echange}. Error: {e}',)
 def printandtelegram(message):
     print(message)
     send_to_telegram(message)
@@ -76,29 +130,30 @@ def get_balance(exchange,symbol):
     if symbol[-5:] == '/USDT':
         symbol = symbol[:-5]
     balance=ex[exchange].fetch_balance()
-    if balance[symbol]['total'] != 0:
-        return balance[symbol]['total']
+    if balance[symbol]['free'] != 0:
+        return balance[symbol]['free']
     else:
         return 0
+    
+ex = {n:getattr(ccxt,n)(exchanges[n]) for n in exchanges}
+
 def get_precision_min(symbol,exchange_str):
     symbol_info = ex[exchange_str].load_markets(symbol)
-    graal = symbol_info[symbol]['limits']['price']['min']
-    return graal
+    grail = symbol_info[symbol]['limits']['price']['min']
+    return grail
 def get_time():
-    # Définir la timezone française
-    tz_france = pytz.timezone('Europe/Paris')
-
-    # Obtenir la date et l'heure actuelles dans la timezone française
-    now = datetime.datetime.now(tz_france)
-
-    # Formater la date et l'heure dans le format souhaité
-    date_heure_format = now.strftime("[%d/%m/%Y  %H:%M:%S]")
-
-    # Retourner la date et l'heure formatées
-    return date_heure_format
+    tz = pytz.timezone(timezone)
+    now = datetime.datetime.now(tz)
+    dtf = now.strftime("[%d/%m/%Y  %H:%M:%S]")
+    return f"{Style.DIM}{dtf}{Style.RESET_ALL}"
+def get_time_blank():
+    tz = pytz.timezone(timezone)
+    now = datetime.datetime.now(tz)
+    dtf = now.strftime("[%d/%m/%Y  %H:%M:%S]")
+    return dtf
 def get_balance_usdt(ex_list_str:list):
     usdt_balance = 0
     for excha in ex_list_str:
         balances = ex[excha].fetchBalance()
-        usdt_balance+=balances['USDT']['total']
+        usdt_balance+=balances['USDT']['free']
     return float(usdt_balance)
