d42c781f76c2888be666df84f4203c66ccc2640f
nelso <87613463+nelso0@users.noreply.github.com>
2024-06-13 01:23:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import subprocess
 import time
-import sys
+import sys,ccxt
 import os
 from colorama import Style, init, Fore
 init()
@@ -19,7 +19,7 @@ VMA""YMM      `7MM"""Yp,      db      `7MM"""Mq.  `7MM"""Yp,   .g8""8q.  MMP""MM
 AMMMMMMMF     .JMMmmmd9 .AMA.   .AMMA..JMML. .JMM..JMMmmmd9    `"bmmd"'     .JMML.    .JMML..JML.    YM  .JMMmmmmMMM 
                                                                                                                      
                                                                                                                      ''')
-print(f" \n{Fore.BLUE}{Style.BRIGHT}DEMO VERSION{Style.RESET_ALL}\n \nnelsorex\nTwitter: @nelsorex\nDiscord: nelsorex\n")
+print(f" \n{Fore.BLUE}{Style.BRIGHT}DEMO VERSION{Style.RESET_ALL}\n \n\nTwitter: @thebarbotine\nDiscord: https://discord.gg/zZpvyz8brn\n")
 args = sys.argv
 mode = args[1]
 if renewal:
@@ -33,27 +33,22 @@ else:
     renew="525600"
     ex_list=args[4]
 i=0
-with open(f"start_balance.txt","w") as f:
-    f.write(balance)
-with open(f"balance.txt","w") as f:
-    f.write(balance)
+if mode!='fake-money':
+    real_balance=0
+    for ex_str in ex_list.split(','):
+        bal = ex[ex_str].fetchBalance()
+        real_balance+=float(bal[symbol.split('/')[1]]['total'])
+    with open(f"real_balance.txt","w") as f:
+        f.write(str(real_balance))
 
 while True:
     if i>=1 and p.returncode==1:
         sys.exit(1)
     if mode == "fake-money":
         p=subprocess.run([how_do_you_usually_launch_python,"bot-fake-money.py",symbol,balance,renew,symbol,ex_list])
-        with open(f"balance.txt") as f:
-            balance=f.read()
-    elif mode == "classic":
-        p=subprocess.run([how_do_you_usually_launch_python,"bot-classic.py",symbol,balance,renew,symbol,ex_list])
-        with open(f"balance.txt") as f:
-            balance=f.read()
-    elif mode == "delta-neutral":
-        p=subprocess.run([how_do_you_usually_launch_python,"bot-delta-neutral.py",symbol,balance,renew,symbol,ex_list])
-        with open(f"balance.txt") as f:
-            balance=f.read()
+    elif mode == "real":
+        p=subprocess.run([how_do_you_usually_launch_python,"bot.py",symbol,balance,renew,symbol,ex_list])
     else:
-        print(f"Mode input is incorrect.")
+        printerror(m=f"mode input is incorrect.")
         sys.exit(1)
     i+=1
