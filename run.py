b198141cd99e9640fa0341883718360c841aef59
nelso <87613463+nelso0@users.noreply.github.com>
2024-06-13 01:23:55 +0200

    Update run.py

--- run.py
+++ run.py
@@ -1,10 +1,10 @@
 import subprocess
 from exchange_config import *
-import sys
+import sys, os
 sys.stdin.reconfigure(encoding="utf-8")
 sys.stdout.reconfigure(encoding="utf-8")
 import time
-from os import path, name,system
+from os import name,system
 from colorama import Style, Fore, init
 init()
 def clear():
@@ -12,27 +12,12 @@ def clear():
       system('cls')
     else:
         system('clear')
-def emergency_convert_list(pair_to_sell,exlist):
-    i=0
-    for echange in exlist:
-        try:
-            if ex[echange].has['cancelAllOrders'] and ex[echange].fetchOpenOrders(pair_to_sell) != []:
-                ex[echange].cancelAllOrders(pair_to_sell)
-                print(f"{Style.DIM}{get_time()}{Style.RESET_ALL} Successfully canceled all orders on {echange}.")
-            bal = get_balance(echange,pair_to_sell)
-            if bal>(float(10)/float(ex[echange].fetch_ticker(pair_to_sell)['last'])):
-                ex[echange].createMarketSellOrder(symbol=pair_to_sell,amount=bal)
-                print(f"{Style.DIM}{get_time()}{Style.RESET_ALL} Successfully sold {bal} {pair_to_sell[:len(pair_to_sell)-5]} on {echange}.")
-            else: print(f"{Style.DIM}{get_time()}{Style.RESET_ALL} Not enough {pair_to_sell[:len(pair_to_sell)-5]} on {echange}.")
-            i+=1
-        except Exception as e:
-            print(f'{Style.DIM}[{time.strftime("%H:%M:%S", time.gmtime(time.time()))}]{Style.RESET_ALL} Problem on {echange}. Error:    {e}')
 
 try:
-    if len(sys.argv) < 2:
-        input_list = ["mode (fake-money, classic, delta-neutral)", "renew time (in minutes)", "balance to use", "symbol", "exchanges list separated without space with commas (,)"]
+    if len(sys.argv) < 3:
+        input_list = ["mode (fake-money or real)", "renewal period (in minutes)", "balance to use", "symbol", "exchanges list separated without space with commas (,)"]
         if not renewal:
-            input_list.remove("renew time (in minutes)")
+            input_list.remove("renewal period (in minutes)")
         output = []
         
         for inputt in input_list:
@@ -50,12 +35,13 @@ try:
             symbol = output[3]
             ex_list = output[4]
 
-        with open(f"start_balance.txt","w") as f:
-            f.write(balance)
-            f.close()
-        with open(f"balance.txt","w") as f:
-            f.write(balance)
-            f.close()
+        if mode!='fake-money':
+            real_balance=0
+            for ex_str in ex_list.split(','):
+                bal = ex[ex_str].fetchBalance()
+                real_balance+=float(bal[symbol.split('/')[1]]['total'])
+            with open(f"real_balance.txt","w") as f:
+                f.write(str(real_balance))
 
         if renewal:
             subprocess.run([how_do_you_usually_launch_python,f"main.py",mode,renew_time,balance,symbol,ex_list])
@@ -63,11 +49,11 @@ try:
             subprocess.run([how_do_you_usually_launch_python,f"main.py",mode,balance,symbol,ex_list])
 
     else:
-        if (len(sys.argv) < 6 or len(sys.argv) > 6) and renewal:
-            print(f"Not correctly configured. Usage:\n \n{how_do_you_usually_launch_python} run.py <mode> <symbol-renew-time> <balance-to-use> <crypto pair> <exchanges list separated without space with commas (,)>\n")
+        if (len(sys.argv) != 6) and renewal:
+            printerror(m=f"Not correctly configured. Usage:\n \n{how_do_you_usually_launch_python} run.py <mode> <renewal period minutes> <balance to use> <crypto pair> <exchanges list separated without space with commas (,)>\n")
             sys.exit(1)
-        if (len(sys.argv) < 5 or len(sys.argv) > 5) and not renewal:
-            print(f"Not correctly configured. Usage:\n \n{how_do_you_usually_launch_python} run.py <mode> <balance-to-use> <crypto pair> <exchanges list separated without space with commas (,)>\n")
+        if (len(sys.argv) != 5) and not renewal:
+            printerror(m=f"Not correctly configured. Usage:\n \n{how_do_you_usually_launch_python} run.py <mode> <balance to use> <crypto pair> <exchanges list separated without space with commas (,)>\n")
             sys.exit(1)
         args = sys.argv
         
@@ -83,12 +69,13 @@ try:
             symbol = args[4]
             ex_list = args[5]
 
-        with open(f"start_balance.txt","w") as f:
-            f.write(balance)
-            f.close()
-        with open(f"balance.txt","w") as f:
-            f.write(balance)
-            f.close()
+        if mode!='fake-money':
+            real_balance=0
+            for ex_str in ex_list.split(','):
+                bal = ex[ex_str].fetchBalance()
+                real_balance+=float(bal[symbol.split('/')[1]]['total'])
+            with open(f"real_balance.txt","w") as f:
+                f.write(str(real_balance))
         
         print('''
                                                                                                                      
@@ -102,40 +89,39 @@ VMA""YMM      `7MM"""Yp,      db      `7MM"""Mq.  `7MM"""Yp,   .g8""8q.  MMP""MM
 AMMMMMMMF     .JMMmmmd9 .AMA.   .AMMA..JMML. .JMM..JMMmmmd9    `"bmmd"'     .JMML.    .JMML..JML.    YM  .JMMmmmmMMM 
                                                                                                                      
                                                                                                                      ''')
-        print(f" \n{Fore.BLUE}{Style.BRIGHT}DEMO VERSION{Style.RESET_ALL}\n \nnelsorex.eth\nTwitter: @nelsorex\nDiscord: nelsorex\n")
+        print(f" \n{Fore.BLUE}{Style.BRIGHT}DEMO VERSION{Style.RESET_ALL}\n \n\nTwitter: @thebarbotine\nDiscord: https://discord.gg/zZpvyz8brn\n")
         i=0
 
         while True:
             if i>=1 and p.returncode==1:
                 sys.exit(1)
             if mode == "fake-money":
-                p=subprocess.run([how_do_you_usually_launch_python,"bot-fake-money.py",symbol,balance,renew_time,symbol,ex_list])
-                with open(f"balance.txt") as f:
-                    balance=f.read()
-            elif mode == "classic":
-                p=subprocess.run([how_do_you_usually_launch_python,"bot-classic.py",symbol,balance,renew_time,symbol,ex_list])
-                with open(f"balance.txt") as f:
-                    balance=f.read()
-            elif mode == "delta-neutral":
-                p=subprocess.run([how_do_you_usually_launch_python,"bot-delta-neutral.py",symbol,balance,renew_time,symbol,ex_list])
-                with open(f"balance.txt") as f:
-                    balance=f.read()
+                if os.path.exists('bot-fake-money.py'):
+                    p=subprocess.run([how_do_you_usually_launch_python,"bot-fake-money.py",symbol,balance,renew_time,symbol,ex_list])
+                else:
+                    printerror(m=f'please put the file "bot-fake-money.py" in the current directory.')
+            elif mode == "real":
+                if os.path.exists('bot.py'):
+                    p=subprocess.run([how_do_you_usually_launch_python,"bot.py",symbol,balance,renew_time,symbol,ex_list])
+                else:
+                    printerror(m=f'please put the file "bot.py" in the current directory.')
             else:
-                print(f"Mode input is incorrect.")
+                printerror(m=f"mode input is incorrect.")
                 sys.exit(1)
             i+=1
 except KeyboardInterrupt:
-    if ctrl_c_handling and mode!='fake-money':
+    if mode!='fake-money':
         print(" \n \n \n")
         clear()
         answered = False
         while answered == False:
-            inp = input(f"{Style.DIM}{get_time()}{Style.RESET_ALL} CTRL+C was pressed. Do you want to sell all crypto back? (y)es / (n)o\n \ninput: ")
-            if inp == "y" or inp == "Y" or inp == "yes" or inp == "Yes":
+            inp = input(f"{get_time()} CTRL+C was pressed. Do you want to sell all crypto back? (y)es / (n)o\n \ninput: ")
+            append_new_line('logs/logs.txt',f"{get_time_blank()} INFO: ctrl+c was pressed.")
+            if inp.lower() == "y" or inp.lower() == "yes":
                 answered = True
                 emergency_convert_list(symbol,[ex_list.split(',')[i] for i in range(len(ex_list.split(',')))])
                 sys.exit(1)
-            if inp == "n" or inp == "N" or inp == "no" or inp == "No":
+            if inp.lower() == "n" or inp.lower() == "no":
                 answered = True
                 sys.exit(1)
             else:
