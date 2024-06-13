94b3fb9ebebb943ffdfdb3d5a59ed72acc428377
nelso <87613463+nelso0@users.noreply.github.com>
2024-06-13 02:22:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -40,8 +40,13 @@ if mode!='fake-money':
         real_balance+=float(bal[symbol.split('/')[1]]['total'])
     with open(f"real_balance.txt","w") as f:
         f.write(str(real_balance))
+else:
+    with open(f"real_balance.txt","w") as f:
+        f.write(str(balance))
 
 while True:
+    with open(f"real_balance.txt","r") as f:
+        balance = float(f.read())
     if i>=1 and p.returncode==1:
         sys.exit(1)
     if mode == "fake-money":
